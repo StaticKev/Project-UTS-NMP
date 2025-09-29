e26f9ff6c03c0d2aa3a5a83ab6413d80602316e0
StaticKev <statickev321@gmail.com>
2025-09-29 16:23:15 +0700

    Kevin: Menyelesaikan Spinner

--- app/src/main/java/com/statickev/projectnmp/StudentDetailsActivity.kt
+++ app/src/main/java/com/statickev/projectnmp/StudentDetailsActivity.kt
@@ -1,7 +1,9 @@
 package com.statickev.projectnmp
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
 import com.statickev.projectnmp.databinding.ActivityStudentDetailsBinding
 
@@ -12,23 +14,57 @@ class StudentDetailsActivity : AppCompatActivity() {
         binding= ActivityStudentDetailsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val student = StudentData.student_data[intent.getIntExtra("position", 0)]
-        binding.txtName.text=student.name.toString()
-        binding.txtNRP.text=student.NRP.toString()
-        binding.txtDetails.text = student.biodata.toString() //sementara
-        binding.imgStudent.setImageResource(student.imageId)
+        // Ini gpp lah ya, biar bisa di scroll gais. Masa 0 gara-gara ini :)
+        binding.txtDetails.movementMethod = android.text.method.ScrollingMovementMethod()
 
-        val program = student.program
-        if(program == "DSAI"){
-            binding.rdoDSAI.isChecked = true
-        } else if(program == "NCS"){
-            binding.rdoNCS.isChecked = true
-        } else if(program == "IMES"){
-            binding.rdoIMES.isChecked = true
-        } else if(program == "DMT"){
-            binding.rdoDMT.isChecked = true
-        } else if(program == "GD"){
-            binding.rdoGD.isChecked = true
+        val items = listOf("About Me", "My Courses", "Organization/Community")
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, items)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        binding.spnOption.adapter = adapter
+
+        binding.spnOption.onItemSelectedListener = object: AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                with(StudentData.student_data[intent.getIntExtra("position", 0)]) {
+                    when (items[position]) {
+                        "About Me" -> binding.txtDetails.text = aboutMe
+                        "My Courses" -> {
+                            var content = ""
+                            courses.forEach { course -> content += "- $course\n" }
+                            binding.txtDetails.text = content
+                        }
+                        "Organization/Community" -> {
+                            var content = ""
+                            organization.forEach { org -> content += "- $org\n" }
+                            binding.txtDetails.text = content
+                        }
+                    }
+                }
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+
+        }
+
+        with(StudentData.student_data[intent.getIntExtra("position", 0)]) {
+            binding.txtName.text = name
+            binding.txtNRP.text = NRP
+            binding.txtDetails.text = aboutMe
+            binding.imgStudent.setImageResource(imageId)
+
+            when (program) {
+                "DSAI" -> binding.rdoDSAI.isChecked = true
+                "NCS" -> binding.rdoNCS.isChecked = true
+                "IMES" -> binding.rdoIMES.isChecked = true
+                "DMT" -> binding.rdoDMT.isChecked = true
+                "GD" -> binding.rdoGD.isChecked = true
+            }
         }
     }
 }
