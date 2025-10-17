8d47720c863400f76980cac71f22f0cb88bd11c8
ShiftorTheOrca <leonardosanjaya23@gmail.com>
2025-10-17 22:15:29 +0700

    Menambahkan fungsionalitas button request

--- app/src/main/java/com/statickev/projectnmp/MainActivity.kt
+++ app/src/main/java/com/statickev/projectnmp/MainActivity.kt
@@ -8,6 +8,10 @@ import com.statickev.projectnmp.databinding.ActivityMainBinding
 class  MainActivity: AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
+    companion object {
+        var FRIEND_COUNT = 0
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
